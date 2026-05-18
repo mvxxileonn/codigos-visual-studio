6fca112882e7edd2c7e5967771f075406729854a
Maximiliano Hernán León Alfaro <mvxxileonn@192.168.1.18>
2026-05-18 12:01:08 -0400

    Cambios

---
+++ MiProyecto/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Ingrese su edad:");
+string input = Console.ReadLine();
+int edad;
+if (int.TryParse(input, out edad))
+{
+    if (edad >= 18)
+    {
+        Console.WriteLine("Eres mayor de edad.");
+    }
+    else
+    {
+        Console.WriteLine("Eres menor de edad.");
+    }
+}
+else
+{
+    Console.WriteLine("Entrada no válida. Por favor, ingresa un número.");
+} 
